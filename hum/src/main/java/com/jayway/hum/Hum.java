09228145ebfb00769992f6956d3dd8bba6a075e4
Tobias Sodergren <tobias.sodergren@jayway.com>
2011-11-22 22:10:15 +0100

    Adding license

--- hum/src/main/java/com/jayway/hum/Hum.java
+++ hum/src/main/java/com/jayway/hum/Hum.java
@@ -20,6 +20,20 @@ import java.util.Set;
  * <li>Configure the Growl application that should produce notifications by calling {@link #registerApplication(String)}</li>
  * <li>Use the {@link com.jayway.hum.api.NotificationProducer}, which is returned when the application is configured, to create notifications.</li>
  * </ol>
+ * <p/>
+ * Copyright 2011 Tobias Södergren
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class Hum implements NotificationProducer {
     private static final String GROWL_APPLICATION = "com.Growl.GrowlHelperApp";
